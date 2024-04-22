22de7fd681dad838ac6f94b00b00066f6cefee80
D A R K <42583742+ace-of-heart@users.noreply.github.com>
2024-04-22 07:54:32 +0000

    kingdom vise page test

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,jsonify
 
 app = Flask(__name__)
+
+KINGDOMS = [
+  "Anuradhapura",
+  "Polonnaruwa",
+  "Dambadeniya",
+  "Seethawaka",
+  "Kotte",
+  "Kandy",
+  "Jaffna"
+]
 
 @app.route('/')
 def indexPage():
@@ -12,7 +22,7 @@ def homePage():
 
 @app.route('/history')
 def history():
-  return render_template('history.html')
+  return render_template('history.html' , kingdoms = KINGDOMS)
 
 @app.route('/inscription')
 def translate():
@@ -25,6 +35,21 @@ def faqs():
 @app.route('/about')
 def about():
   return render_template('about.html')
+  
+@app.route('/history/kingdoms', methods=["POST","GET"])
+def kingdoms():
+  if request.method == "POST":
+      # Handle POST request
+      data = request.json
+      if data and 'kingdom' in data:
+          kingdom = data['kingdom']
+          return render_template(kingdom.lower() + '.html')
+      else:
+          return jsonify({'error': 'Kingdom not specified in the request data'}), 400
+  else:
+    
+      # Handle GET request
+      return render_template(kingdom)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
